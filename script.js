9969f3df09a058161162a7421c41e33e25cdbf68
MarieCoulay <marie.coulay@gmail.com>
2023-11-17 15:49:28 +0100

    boutons animaux

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
-document.getElementById("buttonSwordFight").addEventListener("click", function() {
+document
+  .getElementById("buttonSwordFight")
+  .addEventListener("click", function () {
     var audio = document.getElementById("swordFight");
     audio.play();
-});
+  });
 
-document.getElementById("buttonSwordSlash").addEventListener("click", function() {
+document
+  .getElementById("buttonSwordSlash")
+  .addEventListener("click", function () {
     var audio = document.getElementById("swordSlash");
     audio.play();
+  });
+
+document.getElementById("buttonCatMeow").addEventListener("click", function () {
+  var audio = document.getElementById("catMeow");
+  audio.play();
 });
 
+document
+  .getElementById("buttonCrowSound")
+  .addEventListener("click", function () {
+    var audio = document.getElementById("crowSound");
+    audio.play();
+  });
+
+document
+  .getElementById("buttonDogBarking")
+  .addEventListener("click", function () {
+    var audio = document.getElementById("dogBarking");
+    audio.play();
+  });
+
+document
+  .getElementById("buttonMonkeyScream")
+  .addEventListener("click", function () {
+    var audio = document.getElementById("monkeyScream");
+    audio.play();
+  });
+
+document
+  .getElementById("buttonOwlNoise")
+  .addEventListener("click", function () {
+    var audio = document.getElementById("owlNoise");
+    audio.play();
+  });
+
+document
+  .getElementById("buttonTigerRoar")
+  .addEventListener("click", function () {
+    var audio = document.getElementById("tigerRoar");
+    audio.play();
+  });
+
+document
+  .getElementById("buttonWolfHowl")
+  .addEventListener("click", function () {
+    var audio = document.getElementById("wolfHowl");
+    audio.play();
+  });
