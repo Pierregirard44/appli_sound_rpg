fa093db7bb4be63873de6d31ecbc0283fa35ee84
root <root@LAPTOP-I4U4E0I4>
2023-11-17 12:08:51 +0100

    first real commit with base project

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("buttonSwordFight").addEventListener("click", function() {
+    var audio = document.getElementById("swordFight");
+    audio.play();
+});
+
+document.getElementById("buttonSwordSlash").addEventListener("click", function() {
+    var audio = document.getElementById("swordSlash");
+    audio.play();
+});
+
