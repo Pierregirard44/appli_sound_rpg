2839754010d840d5aa586676dd4df1e378aaa293
root <root@LAPTOP-I4U4E0I4>
2023-11-17 15:44:43 +0100

    human sound/button done

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ document.getElementById("buttonSwordSlash").addEventListener("click", function()
     audio.play();
 });
 
+document.getElementById("buttonLaughDemoniac").addEventListener("click", function() {
+    var audio = document.getElementById("laughDemoniac");
+    audio.play();
+});
+
+document.getElementById("buttonLaughFemale").addEventListener("click", function() {
+    var audio = document.getElementById("laughFemale");
+    audio.play();
+});
+
+document.getElementById("buttonLaughMale").addEventListener("click", function() {
+    var audio = document.getElementById("laughMale");
+    audio.play();
+});
+
+document.getElementById("buttonScreamFemale").addEventListener("click", function() {
+    var audio = document.getElementById("screamFemale");
+    audio.play();
+});
+
+document.getElementById("buttonScreamMale").addEventListener("click", function() {
+    var audio = document.getElementById("screamMale");
+    audio.play();
+});
+
+document.getElementById("buttonWarcryFemale").addEventListener("click", function() {
+    var audio = document.getElementById("warcryFemale");
+    audio.play();
+});
+
+document.getElementById("buttonWarcryMale").addEventListener("click", function() {
+    var audio = document.getElementById("warcryMale");
+    audio.play();
+});
