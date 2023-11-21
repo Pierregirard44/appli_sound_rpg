4ab1ea754ed8628243392fd335830c37dfce4e67
MarieCoulay <marie.coulay@gmail.com>
2023-11-21 15:10:28 +0100

    test

---
+++ rpg_soundbox/src/components/ButtonSound.js
@@ -0,0 +1,138 @@
+const ButtonSound = () => {
+  return (
+    <>
+      {" "}
+      <div id="containerButton">
+        <audio id="swordSlash" class="swordSlash">
+          <source src="./sounds/swordSlash.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonSwordSlash" class="buttonSwordSlash" alt="icon">
+          <img src="./icons/swordSlash.png" alt="icon" />
+        </button>
+
+        <audio id="swordFight">
+          <source src="./sounds/swordFight.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonSwordFight">
+          <img src="./icons/swordFight.png" alt="icon" />
+        </button>
+
+        <audio id="catMeow">
+          <source src="./sounds/catMeow.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonCatMeow">
+          <img src="./icons/catMeow.png" alt="icon" />
+        </button>
+
+        <audio id="crowSound">
+          <source src="./sounds/crowSound.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonCrowSound">
+          <img src="./icons/crowSound.png" alt="icon" />
+        </button>
+
+        <audio id="dogBarking">
+          <source src="./sounds/dogBarking.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonDogBarking">
+          <img src="./icons/dogBarking.png" alt="icon" />
+        </button>
+
+        <audio id="monkeyScream">
+          <source src="./sounds/monkeyScream.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonMonkeyScream">
+          <img src="./icons/monkeyScream.png" alt="icon" />
+        </button>
+
+        <audio id="owlNoise">
+          <source src="./sounds/owlNoise.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonOwlNoise">
+          <img src="./icons/owlNoise.png" alt="icon" />
+        </button>
+
+        <audio id="tigerRoar">
+          <source src="./sounds/tigerRoar.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonTigerRoar">
+          <img src="./icons/tigerRoar.png" alt="icon" />
+        </button>
+
+        <audio id="wolfHowl">
+          <source src="./sounds/wolfHowl.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonWolfHowl">
+          <img src="./icons/wolfHowl.png" alt="icon" />
+        </button>
+
+        <audio id="laughDemoniac">
+          <source src="./sounds/laughDemoniac.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonLaughDemoniac">
+          <img src="./icons/laughDemoniac.png" alt="icon" />
+        </button>
+
+        <audio id="laughFemale">
+          <source src="./sounds/laughFemale.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonLaughFemale">
+          <img src="./icons/laughingFemale.png" alt="icon" />
+        </button>
+
+        <audio id="laughMale">
+          <source src="./sounds/laughMale.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonLaughMale">
+          <img src="./icons/laughMale.png" alt="icon" />
+        </button>
+
+        <audio id="screamFemale">
+          <source src="./sounds/screamFemale.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonScreamFemale">
+          <img src="./icons/screamFemale.png" alt="icon" />
+        </button>
+
+        <audio id="screamMale">
+          <source src="./sounds/screamMale.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonScreamMale">
+          <img src="./icons/screamMale.png" alt="icon" />
+        </button>
+
+        <audio id="warcryFemale">
+          <source src="./sounds/warcryFemale.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonWarcryFemale">
+          <img src="./icons/warcryFemale.png" alt="icon" />
+        </button>
+
+        <audio id="warcryMale">
+          <source src="./sounds/warcryMale.mp3" type="audio/mp3"></source>
+          Votre navigateur ne prend pas en charge l'élément audio.
+        </audio>
+        <button id="buttonWarcryMale">
+          <img src="./icons/warcryMale.png" alt="icon" />
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ButtonSound;
