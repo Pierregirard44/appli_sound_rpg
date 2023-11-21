8d393dc81e2506de0d375032e9b549e9516d25b8
MarieCoulay <marie.coulay@gmail.com>
2023-11-21 15:31:28 +0100

    icons remises en place

--- rpg_soundbox/src/App.js
+++ rpg_soundbox/src/App.js
@@ -2,7 +2,7 @@ import ButtonSound from "./components/ButtonSound";
 
 function App() {
   return (
-    <div className="h-[100vh] bg-slate-900">
+    <div className="h-[100%] bg-slate-900">
       <h1 className="text-gray-50 text-4xl text-center pt-5">RPG Sound</h1>
       <ButtonSound />
     </div>
--- rpg_soundbox/src/components/ButtonSound.js
+++ rpg_soundbox/src/components/ButtonSound.js
@@ -1,3 +1,20 @@
+import catMeow from "../icons/catMeow.png";
+import crowSound from "../icons/crowSound.png";
+import dogBarking from "../icons/dogBarking.png";
+import monkeyScream from "../icons/monkeyScream.png";
+import owlNoise from "../icons/owlNoise.png";
+import tigerRoar from "../icons/tigerRoar.png";
+import wolfHowl from "../icons/wolfHowl.png";
+import laughDemoniac from "../icons/laughDemoniac.png";
+import laughFemale from "../icons/laughingFemale.png";
+import laughMale from "../icons/laughMale.png";
+import screamFemale from "../icons/screamFemale.png";
+import screamMale from "../icons/screamMale.png";
+import warcryFemale from "../icons/warcryFemale.png";
+import warcryMale from "../icons/warcryMale.png";
+import swordSlash from "../icons/swordSlash.png";
+import swordFight from "../icons/swordFight.png";
+
 const ButtonSound = () => {
   return (
     <>
@@ -8,7 +25,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonSwordSlash" class="buttonSwordSlash" alt="icon">
-          <img src="./icons/swordSlash.png" alt="icon" />
+          <img src={swordSlash} alt="icon" />
         </button>
 
         <audio id="swordFight">
@@ -16,7 +33,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonSwordFight">
-          <img src="./icons/swordFight.png" alt="icon" />
+          <img src={swordFight} alt="icon" />
         </button>
 
         <audio id="catMeow">
@@ -24,7 +41,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonCatMeow">
-          <img src="./icons/catMeow.png" alt="icon" />
+          <img src={catMeow} alt="icon" />
         </button>
 
         <audio id="crowSound">
@@ -32,7 +49,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonCrowSound">
-          <img src="./icons/crowSound.png" alt="icon" />
+          <img src={crowSound} alt="icon" />
         </button>
 
         <audio id="dogBarking">
@@ -40,7 +57,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonDogBarking">
-          <img src="./icons/dogBarking.png" alt="icon" />
+          <img src={dogBarking} alt="icon" />
         </button>
 
         <audio id="monkeyScream">
@@ -48,7 +65,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonMonkeyScream">
-          <img src="./icons/monkeyScream.png" alt="icon" />
+          <img src={monkeyScream} alt="icon" />
         </button>
 
         <audio id="owlNoise">
@@ -56,7 +73,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonOwlNoise">
-          <img src="./icons/owlNoise.png" alt="icon" />
+          <img src={owlNoise} alt="icon" />
         </button>
 
         <audio id="tigerRoar">
@@ -64,7 +81,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonTigerRoar">
-          <img src="./icons/tigerRoar.png" alt="icon" />
+          <img src={tigerRoar} alt="icon" />
         </button>
 
         <audio id="wolfHowl">
@@ -72,7 +89,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonWolfHowl">
-          <img src="./icons/wolfHowl.png" alt="icon" />
+          <img src={wolfHowl} alt="icon" />
         </button>
 
         <audio id="laughDemoniac">
@@ -80,7 +97,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonLaughDemoniac">
-          <img src="./icons/laughDemoniac.png" alt="icon" />
+          <img src={laughDemoniac} alt="icon" />
         </button>
 
         <audio id="laughFemale">
@@ -88,7 +105,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonLaughFemale">
-          <img src="./icons/laughingFemale.png" alt="icon" />
+          <img src={laughFemale} alt="icon" />
         </button>
 
         <audio id="laughMale">
@@ -96,7 +113,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonLaughMale">
-          <img src="./icons/laughMale.png" alt="icon" />
+          <img src={laughMale} alt="icon" />
         </button>
 
         <audio id="screamFemale">
@@ -104,7 +121,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonScreamFemale">
-          <img src="./icons/screamFemale.png" alt="icon" />
+          <img src={screamFemale} alt="icon" />
         </button>
 
         <audio id="screamMale">
@@ -112,7 +129,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonScreamMale">
-          <img src="./icons/screamMale.png" alt="icon" />
+          <img src={screamMale} alt="icon" />
         </button>
 
         <audio id="warcryFemale">
@@ -120,7 +137,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonWarcryFemale">
-          <img src="./icons/warcryFemale.png" alt="icon" />
+          <img src={warcryFemale} alt="icon" />
         </button>
 
         <audio id="warcryMale">
@@ -128,7 +145,7 @@ const ButtonSound = () => {
           Votre navigateur ne prend pas en charge l'élément audio.
         </audio>
         <button id="buttonWarcryMale">
-          <img src="./icons/warcryMale.png" alt="icon" />
+          <img src={warcryMale} alt="icon" />
         </button>
       </div>
     </>
