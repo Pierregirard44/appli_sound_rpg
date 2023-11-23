a2ca5c0e7f7541647e0bfe1495e872ba0276508b
MarieCoulay <marie.coulay@gmail.com>
2023-11-23 15:26:04 +0100

    merge

--- rpg_soundbox/src/components/ButtonSound.js
+++ rpg_soundbox/src/components/ButtonSound.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import ButtonCatMeow from './buttonAnimal/ButtonCatMeow';
-import ButtonCrowSound from './buttonAnimal/ButtonCrowSound';
-import ButtonDogBarking from './buttonAnimal/ButtonDogBarking';
-import ButtonMonkeyScream from './buttonAnimal/ButtonMonkeyScream';
-import ButtonOwlNoise from './buttonAnimal/ButtonOwlNoise';
-import ButtonTigerRoar from './buttonAnimal/ButtonTigerRoar';
-import ButtonWolfHowl from './buttonAnimal/ButtonWolfHowl';
-import ButtonLaughDemoniac from './buttonMonster/ButtonLaughDemoniac';  
-import ButtonLaughFemale from './buttonHuman/ButtonLaughFemale';  
+import React from "react";
+import ButtonCatMeow from "./buttonAnimal/ButtonCatMeow";
+import ButtonCrowSound from "./buttonAnimal/ButtonCrowSound";
+import ButtonDogBarking from "./buttonAnimal/ButtonDogBarking";
+import ButtonMonkeyScream from "./buttonAnimal/ButtonMonkeyScream";
+import ButtonOwlNoise from "./buttonAnimal/ButtonOwlNoise";
+import ButtonTigerRoar from "./buttonAnimal/ButtonTigerRoar";
+import ButtonWolfHowl from "./buttonAnimal/ButtonWolfHowl";
+import ButtonLaughDemoniac from "./buttonMonster/ButtonLaughDemoniac";
+import ButtonLaughFemale from "./buttonHuman/ButtonLaughFemale";
 import laughMale from "../icons/laughMale.png";
 import screamFemale from "../icons/screamFemale.png";
 import screamMale from "../icons/screamMale.png";
 import warcryFemale from "../icons/warcryFemale.png";
 import warcryMale from "../icons/warcryMale.png";
-import ButtonSwordFight from './buttonWeapon/ButtonSwordFight';
-import ButtonSwordSlash from './buttonWeapon/ButtonSwordSlash';
+import ButtonSwordFight from "./buttonWeapon/ButtonSwordFight";
+import ButtonSwordSlash from "./buttonWeapon/ButtonSwordSlash";
+import ButtonCricket from "./buttonAnimal/ButtonCricket";
 
 const ButtonSound = () => {
-
   return (
     <>
       {" "}
-
       <div id="containerButtonSound">
         <h2 id="titleCategory">Gros sons de combats sa mère</h2>
         <div id="containerButtonWeapon">
           <ButtonSwordSlash />
           <ButtonSwordFight />
         </div>
-        
+
         <h2 id="titleCategory">Sons de potits animaux Grrr Grrr</h2>
         <div id="containerButtonAnimal">
           <ButtonCatMeow />
@@ -38,6 +37,7 @@ const ButtonSound = () => {
           <ButtonOwlNoise />
           <ButtonTigerRoar />
           <ButtonWolfHowl />
+          <ButtonCricket />
         </div>
 
         <h2 id="titleCategory">And scream and shout and let it out</h2>
@@ -69,7 +69,6 @@ const ButtonSound = () => {
         <div id="containerButtonMonster">
           <ButtonLaughDemoniac />
         </div>
-
       </div>
     </>
   );
---
+++ rpg_soundbox/src/components/buttonAnimal/ButtonCricket.js
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+import cricket from "../../icons/cricket.png";
+import cricketSound from "../../sounds/cricket.mp3";
+
+const MyComponent = () => {
+  const audioRef = useRef(null);
+
+  const playSound = () => {
+    audioRef.current.play();
+    console.log("criii criii");
+  };
+
+  return (
+    <div>
+      <button onClick={playSound}>
+        <img src={cricket} alt="icon" />
+      </button>
+      <audio ref={audioRef}>
+        <source src={cricketSound} type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  );
+};
+
+export default MyComponent;
