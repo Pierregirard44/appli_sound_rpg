faa3ee4a2f93deabc47f869eab8dc1f331057207
MarieCoulay <marie.coulay@gmail.com>
2023-11-24 12:00:56 +0100

    navbar WIP

--- rpg_soundbox/src/App.js
+++ rpg_soundbox/src/App.js
@@ -1,11 +1,11 @@
 import ButtonSound from "./components/ButtonSound";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="h-[100%]">
-      <h1 className="text-gray-50 text-4xl text-center pt-5">
-        RPG SoundBox
-      </h1>
+      <Navbar />
+      <h1 className="text-gray-50 text-4xl text-center pt-5">RPG SoundBox</h1>
       <ButtonSound />
     </div>
   );
