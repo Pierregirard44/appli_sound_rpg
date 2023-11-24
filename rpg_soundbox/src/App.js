e2fadcf453bc441f39144512c742955780490a70
root <root@LAPTOP-I4U4E0I4>
2023-11-24 11:03:35 +0100

    fond ecran

--- rpg_soundbox/src/App.js
+++ rpg_soundbox/src/App.js
@@ -2,7 +2,7 @@ import ButtonSound from "./components/ButtonSound";
 
 function App() {
   return (
-    <div className="h-[100%] bg-slate-900 row-auto">
+    <div className="h-[100%]">
       <h1 className="text-gray-50 text-4xl text-center pt-5">
         RPG SoundBox
       </h1>
