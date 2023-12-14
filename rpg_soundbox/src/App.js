8b4db649a71271d5c587ba4baee72d3add564a14
MarieCoulay <marie.coulay@gmail.com>
2023-12-14 15:39:33 +0100

    répartition par onglet

--- rpg_soundbox/src/App.js
+++ rpg_soundbox/src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-// Importe tes composants spécifiques pour chaque catégorie ici
 import ButtonSound from "./components/ButtonSound";
 import AmbianceComponent from "./components/buttonAmbiance/AmbianceComponent";
 import ArmesComponent from "./components/buttonWeapon/WeaponComponent";
-import MagieComponent from "./components/buttonMagic/MagicComponent";
+import MagicComponent from "./components/buttonMagic/MagicComponent";
 import AnimauxComponent from "./components/buttonAnimal/AnimalComponent";
 import HumainsComponent from "./components/buttonHuman/HumanComponent";
 import MonstresComponent from "./components/buttonMonster/MonsterComponent";
-
-// Importe d'autres composants pour les catégories suivantes...
 
 function App() {
   return (
@@ -20,13 +17,13 @@ function App() {
         <Navbar />
         <h1 className="text-gray-50 text-4xl text-center pt-5">RPG SoundBox</h1>
         <Routes>
-          <Route path="/ambiance" component={AmbianceComponent} />
-          <Route path="/armes" component={ArmesComponent} />
-          <Route path="/magie" component={MagieComponent} />
-          <Route path="/animaux" component={AnimauxComponent} />
-          <Route path="/humains" component={HumainsComponent} />
-          <Route path="/monstres" component={MonstresComponent} />
-          <Route path="/all" component={ButtonSound} />
+          <Route path="/ambiance" element={<AmbianceComponent />} />
+          <Route path="/armes" element={<ArmesComponent />} />
+          <Route path="/magie" element={<MagicComponent />} />
+          <Route path="/animaux" element={<AnimauxComponent />} />
+          <Route path="/humains" element={<HumainsComponent />} />
+          <Route path="/monstres" element={<MonstresComponent />} />
+          <Route path="/all" element={<ButtonSound />} />
         </Routes>
       </Router>
     </div>
