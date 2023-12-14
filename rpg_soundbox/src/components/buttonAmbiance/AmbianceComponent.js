a197e167a240832218292b9b63801b4f7f74354a
Pierregirard44 <girard_pierre@hotmail.fr>
2023-12-14 14:47:12 +0100

    Cat ambiance de faite

---
+++ rpg_soundbox/src/components/buttonAmbiance/AmbianceComponent.js
@@ -0,0 +1,19 @@
+import ButtonBeerPouring from './ButtonBeerPouring';
+import ButtonTavernTrend from './ButtonTavernTrend';
+
+
+const AmbianceAll = () => {
+    return (
+        <div id="containerButtonAmbiance">
+            <h2 id="titleCategory">Banger d'ambiançage medieval style</h2>
+            <div id="containerButtonAmbiance">
+            <ButtonTavernTrend /> 
+            <ButtonBeerPouring />
+            </div>
+        </div>
+    );
+};
+
+export default AmbianceAll;
+
+
