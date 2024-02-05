9167927f7890d3cb8ef26d4ae20f33b164213694
MarieCoulay <marie.coulay@gmail.com>
2024-02-05 10:56:31 +0100

    navbar changes + translation + route changes

--- rpg_soundbox/src/App.js
+++ rpg_soundbox/src/App.js
@@ -20,16 +20,15 @@ function App() {
     <div className="h-[100%]">
       <Router>
         <Navbar />
-        <h1 className="text-gray-50 text-4xl text-center pt-5">RPG SoundBox</h1>
         <Routes>
-          <Route path="/" element={<Navigate to="/all" replace />} />{" "}
-          <Route path="/ambiance" element={<AmbianceComponent />} />
-          <Route path="/armes" element={<ArmesComponent />} />
-          <Route path="/magie" element={<MagicComponent />} />
-          <Route path="/animaux" element={<AnimauxComponent />} />
-          <Route path="/humains" element={<HumainsComponent />} />
-          <Route path="/monstres" element={<MonstresComponent />} />
-          <Route path="/all" element={<ButtonSound />} />
+          <Route path="/" element={<Navigate to="/home" replace />} />{" "}
+          <Route path="/atmosphere" element={<AmbianceComponent />} />
+          <Route path="/weapons" element={<ArmesComponent />} />
+          <Route path="/magic" element={<MagicComponent />} />
+          <Route path="/animals" element={<AnimauxComponent />} />
+          <Route path="/humans" element={<HumainsComponent />} />
+          <Route path="/monsters" element={<MonstresComponent />} />
+          <Route path="/homesoundbox" element={<ButtonSound />} />
         </Routes>
       </Router>
     </div>
--- rpg_soundbox/src/components/Navbar.js
+++ rpg_soundbox/src/components/Navbar.js
@@ -1,57 +1,59 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <nav className="bg-black">
-      <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+      <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
         <Link
-          to="/all"
-          class="border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 mx-1.5 sm:mx-6"
+          to="/"
+          className="border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 mx-1.5 sm:mx-6"
         >
-          Tous les sons
+          Home
         </Link>
 
-        <Link
-          to="/ambiance"
-          class="border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 mx-1.5 sm:mx-6"
-        >
-          Ambiance
-        </Link>
-
-        <Link
-          to="/armes"
-          class="border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 mx-1.5 sm:mx-6"
-        >
-          Armes
-        </Link>
-
-        <Link
-          to="/magie"
-          class="border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 mx-1.5 sm:mx-6"
-        >
-          Magie
-        </Link>
-
-        <Link
-          to="/animaux"
-          class="border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 mx-1.5 sm:mx-6"
-        >
-          Animaux
-        </Link>
-        <Link
-          to="/humains"
-          class="border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 mx-1.5 sm:mx-6"
-        >
-          Humains
-        </Link>
-        <Link
-          to="/monstres"
-          class="border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 mx-1.5 sm:mx-6"
-        >
-          Monstres
-        </Link>
+        <div className="relative mx-1.5 sm:mx-6">
+          <span
+            onClick={toggleDropdown}
+            className="cursor-pointer border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500"
+          >
+            Soundbox
+          </span>
+          {showDropdown && (
+            <div className="absolute bg-black text-gray-600 mt-1 p-2 rounded-md shadow-lg">
+              <Link to="/homesoundbox" className="block hover:text-white">
+                All sounds
+              </Link>
+              <Link to="/atmosphere" className="block hover:text-white">
+                Atmosphere
+              </Link>
+              <Link to="/weapons" className="block hover:text-white">
+                Weapons
+              </Link>
+              <Link to="/magic" className="block hover:text-white">
+                Magic
+              </Link>
+              <Link to="/animals" className="block hover:text-white">
+                Animal
+              </Link>
+              <Link to="/humans" className="block hover:text-white">
+                Humans
+              </Link>
+              <Link to="/monsters" className="block hover:text-white">
+                Monsters
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
 };
+
 export default Navbar;
