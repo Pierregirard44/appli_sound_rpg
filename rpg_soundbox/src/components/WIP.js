cabf4ac6d9d60a022d82129a316fc9bee3e805cb
Pierregirard44 <girard_pierre@hotmail.fr>
2024-02-05 13:40:04 +0100

    WIP page working

--- rpg_soundbox/src/components/WIP.js
+++ rpg_soundbox/src/components/WIP.js
@@ -5,7 +5,8 @@ const WIP = () => {
 
   return (
     <div className="wip-container">
-        <h1>Work in progress</h1>
+        <img className="wip-img" src="https://cdn-icons-png.flaticon.com/512/8090/8090268.png" />
+        <img className="wip-gif" src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcm8zYWZyYno4ZmV4OGJybzhzNW5pbTRocHpjczdyeW05bWRkd2hxbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eti1qm33pRbaw/giphy.gif" alt="work in progress" />
     </div>
   );
 };
