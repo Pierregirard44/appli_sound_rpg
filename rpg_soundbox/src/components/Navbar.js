f09fea0e2fc89b952176013795ed0f0de5734857
MarieCoulay <marie.coulay@gmail.com>
2024-02-05 11:37:44 +0100

    each link in navbar has its own dropdown + UI fixed

--- rpg_soundbox/src/components/Navbar.js
+++ rpg_soundbox/src/components/Navbar.js
@@ -2,18 +2,46 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
+  const [showSoundboxDropdown, setShowSoundboxDropdown] = useState(false);
+  const [showCharacterSheetDropdown, setShowCharacterSheetDropdown] =
+    useState(false);
+  const [showLibraryDropdown, setShowLibraryDropdown] = useState(false);
   const [dropdownTimeout, setDropdownTimeout] = useState(null);
 
-  const showDropdownHandler = () => {
-    setShowDropdown(true);
+  const showDropdownHandler = (dropdown) => {
     clearTimeout(dropdownTimeout); // Clear any existing timeout
+
+    switch (dropdown) {
+      case "soundbox":
+        setShowSoundboxDropdown(true);
+        break;
+      case "characterSheet":
+        setShowCharacterSheetDropdown(true);
+        break;
+      case "library":
+        setShowLibraryDropdown(true);
+        break;
+      default:
+        break;
+    }
   };
 
-  const hideDropdownHandler = () => {
-    // Set a timeout to hide the dropdown after a delay (e.g., 300 milliseconds)
+  const hideDropdownHandler = (dropdown) => {
+    // Set a timeout to hide the dropdown after a delay (e.g., 100 milliseconds)
     const timeout = setTimeout(() => {
-      setShowDropdown(false);
+      switch (dropdown) {
+        case "soundbox":
+          setShowSoundboxDropdown(false);
+          break;
+        case "characterSheet":
+          setShowCharacterSheetDropdown(false);
+          break;
+        case "library":
+          setShowLibraryDropdown(false);
+          break;
+        default:
+          break;
+      }
     }, 100);
 
     setDropdownTimeout(timeout);
@@ -31,43 +59,69 @@ const Navbar = () => {
 
         <div
           className="relative mx-1.5 sm:mx-6"
-          onMouseEnter={showDropdownHandler}
-          onMouseLeave={hideDropdownHandler}
+          onMouseEnter={() => showDropdownHandler("soundbox")}
+          onMouseLeave={() => hideDropdownHandler("soundbox")}
         >
-          <span className="cursor-pointer border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500">
+          <span className="cursor-pointer border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 ml-4">
             Soundbox
           </span>
-          {showDropdown && (
+          {showSoundboxDropdown && (
             <div className="absolute bg-black text-gray-600 mt-1 p-2 rounded-md shadow-lg">
-              <Link to="/homesoundbox" className="block hover:text-white">
+              <Link to="/homesoundbox" className="block hover:text-white ml-2">
                 All sounds
               </Link>
-              <Link to="/atmosphere" className="block hover:text-white">
+              <Link to="/atmosphere" className="block hover:text-white ml-2">
                 Atmosphere
               </Link>
-              <Link to="/weapons" className="block hover:text-white">
+              <Link to="/weapons" className="block hover:text-white ml-2">
                 Weapons
               </Link>
-              <Link to="/magic" className="block hover:text-white">
+              <Link to="/magic" className="block hover:text-white ml-2">
                 Magic
               </Link>
-              <Link to="/animals" className="block hover:text-white">
+              <Link to="/animals" className="block hover:text-white ml-2">
                 Animal
               </Link>
-              <Link to="/humans" className="block hover:text-white">
+              <Link to="/humans" className="block hover:text-white ml-2">
                 Humans
               </Link>
-              <Link to="/monsters" className="block hover:text-white">
+              <Link to="/monsters" className="block hover:text-white ml-2">
                 Monsters
               </Link>
             </div>
           )}
-          <span className="cursor-pointer border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500">
+        </div>
+
+        <div
+          className="relative mx-1.5 sm:mx-6"
+          onMouseEnter={() => showDropdownHandler("characterSheet")}
+          onMouseLeave={() => hideDropdownHandler("characterSheet")}
+        >
+          <span className="cursor-pointer border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 ml-4">
             Character sheet
           </span>
-          <span className="cursor-pointer border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500">
+          {showCharacterSheetDropdown && (
+            <div className="absolute bg-black text-gray-600 mt-1 p-2 rounded-md shadow-lg">
+              {/* Contenu spécifique du dropdown pour Character Sheet */}
+              {/* ... */}
+            </div>
+          )}
+        </div>
+
+        <div
+          className="relative mx-1.5 sm:mx-6"
+          onMouseEnter={() => showDropdownHandler("library")}
+          onMouseLeave={() => hideDropdownHandler("library")}
+        >
+          <span className="cursor-pointer border-b-2 border-transparent hover:text-white dark:hover:text-gray-200 hover:border-orange-500 ml-4">
             Library
           </span>
+          {showLibraryDropdown && (
+            <div className="absolute bg-black text-gray-600 mt-1 p-2 rounded-md shadow-lg">
+              {/* Contenu spécifique du dropdown pour Library */}
+              {/* ... */}
+            </div>
+          )}
         </div>
       </div>
     </nav>
