f59f7a0363dffa313c2cb8018d6dfa8bf72d37c0
Pierregirard44 <girard_pierre@hotmail.fr>
2024-02-05 12:03:43 +0100

    center and right WIP div

--- rpg_soundbox/src/components/WIP.js
+++ rpg_soundbox/src/components/WIP.js
@@ -1,9 +1,10 @@
 import React, { useRef } from 'react';
+import '../App.css';
 
 const WIP = () => {
 
   return (
-    <div>
+    <div className="wip-container">
         <h1>Work in progress</h1>
     </div>
   );
