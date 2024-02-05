1b87ec4df13fe945f5aa528d5205bc82ab4d5017
MarieCoulay <marie.coulay@gmail.com>
2024-02-05 13:24:05 +0100

    logo added to navbar + links justified end

--- rpg_soundbox/src/components/Navbar.js
+++ rpg_soundbox/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import "../fonts/GoudyBookletter1911.otf";
+import logo from "../images/logo_golden.png";
 
 const Navbar = () => {
   const [showSoundboxDropdown, setShowSoundboxDropdown] = useState(false);
@@ -53,8 +54,13 @@ const Navbar = () => {
     <nav className="bg-red-950">
       <div
         id="navbar"
-        className="container flex items-center justify-center p-6 mx-auto capitalize dark:text-amber-500 font-family:Goudy"
+        className="container flex items-center justify-center p-2 mx-auto capitalize dark:text-amber-500 font-family:Goudy"
       >
+        {/* Créez une div pour le logo avec une classe spécifique */}
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="h-16 w-16" />
+        </div>
+
         <Link
           to="/"
           className="border-b-2 border-transparent hover:border-orange-500 mx-1.5 sm:mx-6"
