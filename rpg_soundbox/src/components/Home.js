2ca1a8b405fd654593ff1b58736c6262e5170615
MarieCoulay <marie.coulay@gmail.com>
2024-02-26 11:22:37 +0100

    upgrade 2 on responsive navbar

--- rpg_soundbox/src/components/Home.js
+++ rpg_soundbox/src/components/Home.js
@@ -3,21 +3,59 @@ import "../App.css";
 const Home = () => {
   return (
     <div className="home-container">
-        <div className="home-content">
-          <h1><b>Welcome to "RPG Soundbox"!</b></h1>
-          <p>A collaborative creation by two aspiring developers, Pierre and Marie, born out of a "One Day, One App" challenge.</p>
-          <h2><b>🎲 Immerse in Adventure:</b></h2>
-          <p>Pierre's audio wizardry brings our Soundbox to life. Dive into a world of atmospheric sounds, from echoing caves to mystical chants, enhancing your RPG adventures.</p>
-          <h2><b>🧙‍♂️ Craft Your Character:</b></h2>
-          <p>Marie's coding finesse powers our Character Creation. Shape a unique RPG alter ego effortlessly – choose your race, class, and background with seamless precision.</p>
-          <h2><b>📚 Unlock Resources:</b></h2>
-          <p>Explore a curated collection of RPG resources. Pierre and Marie's dedication ensures a wealth of articles and tools to enhance your storytelling and gameplay.</p>
-          <h2><b>🛠️ About Our Developers:</b></h2>
-          <p>Meet Pierre, the sonic sorcerer, and Marie, the coding conjurer. Together, they bring their passion for audio and development to make your RPG experience unforgettable.</p>
-          <h2><b>🚀 Embark on Your RPG Journey:</b></h2>
-          <p>Join us on "RPG Soundbox" for an immersive and seamless RPG experience. Pierre and Marie invite you to forge epic tales with every click and code.</p>
-          <p>Gear up, adventurer! Your "RPG Soundbox" adventure awaits. May your quests be legendary! 🌟</p>
-        </div>
+      <div className="home-content">
+        <h1>
+          <b>Welcome to "RPG Soundbox"!</b>
+        </h1>
+        <p>
+          A collaborative creation by two aspiring developers, Pierre and Marie,
+          born out of a "One Day, One App" challenge.
+        </p>
+        <h2>
+          <b>🎲 Immerse in Adventure:</b>
+        </h2>
+        <p>
+          Pierre's audio wizardry brings our Soundbox to life. Dive into a world
+          of atmospheric sounds, from echoing caves to mystical chants,
+          enhancing your RPG adventures.
+        </p>
+        <h2>
+          <b>🧙‍♂️ Craft Your Character:</b>
+        </h2>
+        <p>
+          Marie's coding finesse powers our Character Creation. Shape a unique
+          RPG alter ego effortlessly – choose your race, class, and background
+          with seamless precision.
+        </p>
+        <h2>
+          <b>📚 Unlock Resources:</b>
+        </h2>
+        <p>
+          Explore a curated collection of RPG resources. Pierre and Marie's
+          dedication ensures a wealth of articles and tools to enhance your
+          storytelling and gameplay.
+        </p>
+        <h2>
+          <b>🛠️ About Our Developers:</b>
+        </h2>
+        <p>
+          Meet Pierre, the sonic sorcerer, and Marie, the coding conjurer.
+          Together, they bring their passion for audio and development to make
+          your RPG experience unforgettable.
+        </p>
+        <h2>
+          <b>🚀 Embark on Your RPG Journey:</b>
+        </h2>
+        <p>
+          Join us on "RPG Soundbox" for an immersive and seamless RPG
+          experience. Pierre and Marie invite you to forge epic tales with every
+          click and code.
+        </p>
+        <p>
+          Gear up, adventurer! Your "RPG Soundbox" adventure awaits. May your
+          quests be legendary! 🌟
+        </p>
+      </div>
     </div>
   );
 };
