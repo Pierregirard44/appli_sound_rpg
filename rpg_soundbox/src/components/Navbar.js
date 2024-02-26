7844b2a25ce258148bf9f1a3c30f9001895dfa54
MarieCoulay <marie.coulay@gmail.com>
2024-02-26 12:41:57 +0100

    menu burger WIP

--- rpg_soundbox/src/components/Navbar.js
+++ rpg_soundbox/src/components/Navbar.js
@@ -11,6 +11,7 @@ const Navbar = () => {
     useState(false);
   const [showLibraryDropdown, setShowLibraryDropdown] = useState(false);
   const [dropdownTimeout, setDropdownTimeout] = useState(null);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const showDropdownHandler = (dropdown) => {
     clearTimeout(dropdownTimeout); // Clear any existing timeout
@@ -53,111 +54,213 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div
-        id="navbar"
-        className="container flex items-center justify-center p-1 mx-auto capitalize dark:text-amber-500 font-family:Goudy"
-      >
-        {/* Créez une div pour le logo avec une classe spécifique */}
-        <div className="logo-container flex items-center">
-          <img src={logo} alt="Logo" className="h-16 w-16" />
-          <div
-            id="title"
-            className="ml-4 container flex items-center justify-center p-2 capitalize dark:text-amber-500 font-family:Goudy"
+      <div id="bigScreen" className="hidden lg:block">
+        <div
+          id="navbar"
+          className="container flex items-center justify-center p-1 mx-auto capitalize dark:text-amber-500 font-family:Goudy"
+        >
+          {/* Créez une div pour le logo avec une classe spécifique */}
+          <div className="logo-container flex items-center">
+            <img src={logo} alt="Logo" className="h-16 w-16" />
+            <div
+              id="title"
+              className="ml-4 container flex items-center justify-center p-2 capitalize dark:text-amber-500 font-family:Goudy"
+            >
+              RPG Soundbox
+            </div>
+          </div>
+
+          <Link
+            to="/"
+            className="border-b-2 border-transparent hover:border-orange-500 mx-1.5 sm:mx-6"
           >
-            RPG Soundbox
+            Home
+          </Link>
+
+          <div
+            className="relative mx-1.5 sm:mx-6"
+            onMouseEnter={() => showDropdownHandler("soundbox")}
+            onMouseLeave={() => hideDropdownHandler("soundbox")}
+          >
+            <span className="cursor-pointer border-b-2 border-transparent hover:border-orange-500 ml-4">
+              Soundbox{" "}
+              <img src={triangle} alt="triangle" className="h-3 w-3 inline" />
+            </span>
+            {showSoundboxDropdown && (
+              <div className="absolute  bg-red-900 dark:text-amber-500 mt-1 p-2 rounded-md shadow-lg">
+                <Link
+                  to="/homesoundbox"
+                  className="block hover:text-white ml-2"
+                >
+                  All sounds
+                </Link>
+                <Link to="/atmosphere" className="block hover:text-white ml-2">
+                  Atmosphere
+                </Link>
+                <Link to="/weapons" className="block hover:text-white ml-2">
+                  Weapons
+                </Link>
+                <Link to="/magic" className="block hover:text-white ml-2">
+                  Magic
+                </Link>
+                <Link to="/animals" className="block hover:text-white ml-2">
+                  Animal
+                </Link>
+                <Link to="/humans" className="block hover:text-white ml-2">
+                  Humans
+                </Link>
+                <Link to="/monsters" className="block hover:text-white ml-2">
+                  Monsters
+                </Link>
+              </div>
+            )}
+          </div>
+
+          <div
+            className="relative mx-1.5 sm:mx-6"
+            onMouseEnter={() => showDropdownHandler("characterSheet")}
+            onMouseLeave={() => hideDropdownHandler("characterSheet")}
+          >
+            <span className="cursor-pointer border-b-2 border-transparent   hover:border-orange-500 ml-4">
+              Character sheet{" "}
+              <img src={triangle} alt="triangle" className="h-3 w-3 inline" />
+            </span>
+            {showCharacterSheetDropdown && (
+              <div className="absolute  bg-red-900 dark:text-amber-500 mt-1 p-2 rounded-md shadow-lg">
+                <Link to="/WIP" className="block hover:text-white ml-2">
+                  New character
+                </Link>
+                <Link to="/WIP" className="block hover:text-white ml-2">
+                  Character list
+                </Link>
+              </div>
+            )}
+          </div>
+
+          <div
+            className="relative mx-1.5 sm:mx-6"
+            onMouseEnter={() => showDropdownHandler("library")}
+            onMouseLeave={() => hideDropdownHandler("library")}
+          >
+            <span className="cursor-pointer border-b-2 border-transparent   hover:border-orange-500 ml-4">
+              Library{" "}
+              <img src={triangle} alt="triangle" className="h-3 w-3 inline" />
+            </span>
+            {showLibraryDropdown && (
+              <div className="absolute  bg-red-900 dark:text-amber-500 mt-1 p-2 rounded-md shadow-lg">
+                <Link to="/WIP" className="block hover:text-white ml-2">
+                  Spells
+                </Link>
+                <Link to="/WIP" className="block hover:text-white ml-2">
+                  Items
+                </Link>
+                <Link to="/WIP" className="block hover:text-white ml-2">
+                  Monsters
+                </Link>
+                <Link to="/WIP" className="block hover:text-white ml-2">
+                  Weapons
+                </Link>
+                <Link to="/WIP" className="block hover:text-white ml-2">
+                  Armors
+                </Link>
+              </div>
+            )}
           </div>
         </div>
+      </div>
 
-        <Link
-          to="/"
-          className="border-b-2 border-transparent hover:border-orange-500 mx-1.5 sm:mx-6"
-        >
-          Home
-        </Link>
-
+      <div id="smallScreen" className="lg:hidden">
         <div
-          className="relative mx-1.5 sm:mx-6"
-          onMouseEnter={() => showDropdownHandler("soundbox")}
-          onMouseLeave={() => hideDropdownHandler("soundbox")}
+          id="navbar"
+          className="container flex items-center justify-between p-1 mx-auto capitalize dark:text-amber-500 font-family:Goudy"
         >
-          <span className="cursor-pointer border-b-2 border-transparent hover:border-orange-500 ml-4">
-            Soundbox{" "}
-            <img src={triangle} alt="triangle" className="h-3 w-3 inline" />
-          </span>
-          {showSoundboxDropdown && (
-            <div className="absolute  bg-red-900 dark:text-amber-500 mt-1 p-2 rounded-md shadow-lg">
-              <Link to="/homesoundbox" className="block hover:text-white ml-2">
-                All sounds
-              </Link>
-              <Link to="/atmosphere" className="block hover:text-white ml-2">
-                Atmosphere
-              </Link>
-              <Link to="/weapons" className="block hover:text-white ml-2">
-                Weapons
-              </Link>
-              <Link to="/magic" className="block hover:text-white ml-2">
-                Magic
-              </Link>
-              <Link to="/animals" className="block hover:text-white ml-2">
-                Animal
-              </Link>
-              <Link to="/humans" className="block hover:text-white ml-2">
-                Humans
-              </Link>
-              <Link to="/monsters" className="block hover:text-white ml-2">
-                Monsters
-              </Link>
+          <div className="logo-container flex items-center">
+            <img src={logo} alt="Logo" className="h-16 w-16" />
+            <div
+              id="title"
+              className="ml-4 container flex items-center justify-center p-2 capitalize dark:text-amber-500 font-family:Goudy"
+            >
+              RPG Soundbox
             </div>
-          )}
-        </div>
+          </div>
 
-        <div
-          className="relative mx-1.5 sm:mx-6"
-          onMouseEnter={() => showDropdownHandler("characterSheet")}
-          onMouseLeave={() => hideDropdownHandler("characterSheet")}
-        >
-          <span className="cursor-pointer border-b-2 border-transparent   hover:border-orange-500 ml-4">
-            Character sheet{" "}
-            <img src={triangle} alt="triangle" className="h-3 w-3 inline" />
-          </span>
-          {showCharacterSheetDropdown && (
-            <div className="absolute  bg-red-900 dark:text-amber-500 mt-1 p-2 rounded-md shadow-lg">
-              <Link to="/WIP" className="block hover:text-white ml-2">
-                New character
-              </Link>
-              <Link to="/WIP" className="block hover:text-white ml-2">
-                Character list
-              </Link>
-            </div>
-          )}
-        </div>
+          {/* Bouton Burger */}
+          <button
+            onClick={() => setMenuOpen(!isMenuOpen)}
+            className="text-amber-500 cursor-pointer focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isMenuOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              )}
+            </svg>
+          </button>
 
-        <div
-          className="relative mx-1.5 sm:mx-6"
-          onMouseEnter={() => showDropdownHandler("library")}
-          onMouseLeave={() => hideDropdownHandler("library")}
-        >
-          <span className="cursor-pointer border-b-2 border-transparent   hover:border-orange-500 ml-4">
-            Library{" "}
-            <img src={triangle} alt="triangle" className="h-3 w-3 inline" />
-          </span>
-          {showLibraryDropdown && (
-            <div className="absolute  bg-red-900 dark:text-amber-500 mt-1 p-2 rounded-md shadow-lg">
-              <Link to="/WIP" className="block hover:text-white ml-2">
-                Spells
-              </Link>
-              <Link to="/WIP" className="block hover:text-white ml-2">
-                Items
-              </Link>
-              <Link to="/WIP" className="block hover:text-white ml-2">
-                Monsters
-              </Link>
-              <Link to="/WIP" className="block hover:text-white ml-2">
-                Weapons
-              </Link>
-              <Link to="/WIP" className="block hover:text-white ml-2">
-                Armors
-              </Link>
+          {/* Menu */}
+          {isMenuOpen && (
+            <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+              {/* Ajoute ici tes éléments de menu */}
+              <ul className="text-white">
+                <li className="py-2">
+                  <Link to="/" className="hover:text-orange-500">
+                    Home
+                  </Link>
+                </li>
+                <li className="py-2">
+                  <Link to="/homesoundbox" className="hover:text-orange-500">
+                    All sounds
+                  </Link>
+                </li>
+                <li className="py-2">
+                  <Link to="/atmosphere" className="hover:text-orange-500">
+                    Atmosphere
+                  </Link>
+                </li>
+                <li className="py-2">
+                  <Link to="/weapons" className="hover:text-orange-500">
+                    Weapons
+                  </Link>
+                </li>
+                <li className="py-2">
+                  <Link to="/magic" className="hover:text-orange-500">
+                    Magic
+                  </Link>
+                </li>
+                <li className="py-2">
+                  <Link to="/animals" className="hover:text-orange-500">
+                    Animal
+                  </Link>
+                </li>
+                <li className="py-2">
+                  <Link to="/humans" className="hover:text-orange-500">
+                    Humans
+                  </Link>
+                </li>
+                <li className="py-2">
+                  <Link to="/monsters" className="hover:text-orange-500">
+                    Monsters
+                  </Link>
+                </li>
+                {/* Ajoute d'autres liens ici en suivant le même format */}
+              </ul>
             </div>
           )}
         </div>
